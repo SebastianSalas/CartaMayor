e1a3bebf43a76927ce84864c6a5aab6603ba1974
SebastianSalas <janiersalas09@gmail.com>
2021-12-20 22:52:57 -0500

    Finished application.

--- src/cartaMayor/GUI.java
+++ src/cartaMayor/GUI.java
@@ -1,4 +1,5 @@
 package cartaMayor;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -6,9 +7,10 @@ import java.awt.event.ActionListener;
 
 /**
  * This class is used for ...
- * @autor Jhon Alexander Valencia Hilamo jhon.hilamo@correounivalle.edu.co and
- *        Janiert Sebastian Salas Castillo janiert.salas@correounivalle.edu.co
+ *
  * @version v.1.0.0 date:21/11/2021
+ * @autor Jhon Alexander Valencia Hilamo jhon.hilamo@correounivalle.edu.co and
+ * Janiert Sebastian Salas Castillo janiert.salas@correounivalle.edu.co
  */
 public class GUI extends JFrame {
 
@@ -16,12 +18,12 @@ public class GUI extends JFrame {
             "Oprime el botón lanzar para iniciar el juego" +
             "\nSi tu carta es mayor que la de la maquina Ganaras!!" +
             "\nSi tu Carta es menor que la de la maquina Perderas!!" +
-            "\nSi tu carta es igual que la de la maquina, el ganador se decedira por los palos" +
+            "\nSi tu carta es igual que la de la maquina, el ganador se decidirá por el mayor valor del palo" +
             "\nOros es mayor que Copas, Copas es mayor que Espadas y Espadas es mayor que Bastos";
     private Header headerProject;
     private JLabel cartaJ, cartaM;
     private JButton lanzar, ayuda, salir;
-    private JPanel panelCartaJ,panelCartaM;
+    private JPanel panelCartaJ, panelCartaM;
     private ImageIcon imageCarta;
     private JTextArea mensajeSalida, resultadosDados;
     private Escucha escucha;
@@ -30,12 +32,12 @@ public class GUI extends JFrame {
     /**
      * Constructor of GUI class
      */
-    public GUI(){
+    public GUI() {
         initGUI();
 
         //Default JFrame configuration
         this.setTitle("Carta Mayor");
-        this.setSize(200,100);
+        this.setSize(200, 100);
         this.pack();
         //this.setUndecorated(true);
         this.setResizable(true);
@@ -58,82 +60,89 @@ public class GUI extends JFrame {
         model = new Model();
         //Set up JComponents
         headerProject = new Header("Juego Carta Mayor", Color.BLACK);
-        constraints.gridx=0;
-        constraints.gridy=0;
-        constraints.gridwidth=2;
-        constraints.fill=GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
 
-        this.add(headerProject,constraints);
+        this.add(headerProject, constraints);
 
         ayuda = new JButton(" ? ");
         ayuda.addActionListener(escucha);
-        constraints.gridx=0;
-        constraints.gridy=1;
-        constraints.gridwidth=1;
-        constraints.fill=GridBagConstraints.NONE;
-        constraints.anchor=GridBagConstraints.LINE_START;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.LINE_START;
 
-        this.add(ayuda,constraints);
+        this.add(ayuda, constraints);
 
         salir = new JButton(" X ");
         salir.addActionListener(escucha);
-        constraints.gridx=1;
-        constraints.gridy=1;
-        constraints.gridwidth=1;
-        constraints.fill=GridBagConstraints.NONE;
-        constraints.anchor=GridBagConstraints.LINE_END;
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.LINE_END;
 
-        this.add(salir,constraints);
+        this.add(salir, constraints);
+
+        imageCarta= new ImageIcon(getClass().getResource("/resources/0.png"));
+        cartaJ= new JLabel(imageCarta);
+        cartaM= new JLabel(imageCarta);
 
         panelCartaJ = new JPanel();
-        panelCartaJ.setPreferredSize(new Dimension(300,180));
+        panelCartaJ.setPreferredSize(new Dimension(300, 180));
         panelCartaJ.setBorder(BorderFactory.createTitledBorder("Tu Carta "));
-        constraints.gridx=0;
-        constraints.gridy=2;
-        constraints.gridwidth=1;
-        constraints.fill=GridBagConstraints.BOTH;
-        constraints.anchor=GridBagConstraints.CENTER;
-        add(panelCartaJ,constraints);
+        panelCartaJ.add(cartaJ);
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(panelCartaJ, constraints);
 
         panelCartaM = new JPanel();
-        panelCartaM.setPreferredSize(new Dimension(300,180));
+        panelCartaM.setPreferredSize(new Dimension(300, 180));
         panelCartaM.setBorder(BorderFactory.createTitledBorder("Carta maquina "));
-        constraints.gridx=1;
-        constraints.gridy=2;
-        constraints.gridwidth=1;
-        constraints.fill=GridBagConstraints.BOTH;
-        constraints.anchor=GridBagConstraints.CENTER;
-        add(panelCartaM,constraints);
+        panelCartaM.add(cartaM);
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(panelCartaM, constraints);
 
         lanzar = new JButton("Sacar carta");
         lanzar.addActionListener(escucha);
-        constraints.gridx=0;
-        constraints.gridy=3;
-        constraints.gridwidth=2;
-        constraints.fill=GridBagConstraints.NONE;
-        constraints.anchor=GridBagConstraints.CENTER;
-        add(lanzar,constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(lanzar, constraints);
 
-        mensajeSalida = new JTextArea(4,31);
+        mensajeSalida = new JTextArea(4, 31);
         mensajeSalida.setText("Usa el boton (?) para ver las reglas del juego");
         mensajeSalida.setEditable(false);
         mensajeSalida.setBackground(null);
         mensajeSalida.setBorder(BorderFactory.createTitledBorder("Mensajes"));
-        constraints.gridx=0;
-        constraints.gridy=4;
-        constraints.gridwidth=2;
-        constraints.fill=GridBagConstraints.NONE;
-        constraints.anchor=GridBagConstraints.CENTER;
-        add(mensajeSalida,constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(mensajeSalida, constraints);
         //Change this line if you change JFrame Container's Layout
     }
 
     /**
      * Main process of the Java program
+     *
      * @param args Object used in order to send input data from command line when
      *             the program is execute by console.
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             GUI miProjectGUI = new GUI();
         });
@@ -150,14 +159,16 @@ public class GUI extends JFrame {
              */
             if (e.getSource() == lanzar) {
                 model.calcularResultado();
-                //int[] caras = modelCraps.getCaras();
-                //modelCraps.determinarJuego();
-                //resultadosDados.setText(modelCraps.getEstadoToString()[0]);
-                //mensajeSalida.setText(modelCraps.getEstadoToString()[1]);
-            }else{
-                if(e.getSource()==ayuda){
-                    JOptionPane.showMessageDialog(null,MENSAJE_INICIO);
-                }else{
+                int[] cartas = model.getCartas();
+                imageCarta = new ImageIcon(getClass().getResource("/resources/" + cartas[0] + cartas[1] + ".png"));
+                cartaJ.setIcon(imageCarta);
+                imageCarta = new ImageIcon(getClass().getResource("/resources/" + cartas[2] + cartas[3] + ".png"));
+                cartaM.setIcon(imageCarta);
+                mensajeSalida.setText(model.getEstadoToString());
+            } else {
+                if (e.getSource() == ayuda) {
+                    JOptionPane.showMessageDialog(null, MENSAJE_INICIO);
+                } else {
                     System.exit(0);
                 }
             }
--- src/cartaMayor/Model.java
+++ src/cartaMayor/Model.java
@@ -1,46 +1,51 @@
 package cartaMayor;
+
 /**
  * Model apply rules
  * estado 1= Machine winne
  * estado 2= Players winner
  * estado 3= draw
+ *
  * @author Alexander Valencia
  */
 public class Model {
-    private Carta turnoJ,turnoM,paloJ, paloM;
+    private Carta turnoJ, turnoM, paloJ, paloM;
     private int[] cartas;
     private String estadoToString;
     private int estado, estadoPalo;
 
 
-
-    public Model(){
+    public Model() {
         turnoJ = new Carta();
         turnoM = new Carta();
         paloJ = new Carta();
         paloM = new Carta();
         cartas = new int[4];
-
         estadoToString = new String();
     }
 
 
-    public void calcularResultado(){
-        cartas[0]=turnoJ.getNum();
-        cartas[1]=paloJ.getPalo();
-        cartas[2]=turnoM.getNum();
-        cartas[3]=paloM.getPalo();
+    public void calcularResultado() {
+        cartas[0] = turnoJ.getNum();
+        cartas[1] = paloJ.getPalo();
+        cartas[2] = turnoM.getNum();
+        cartas[3] = paloM.getPalo();
 
-        //Machine win
-        if (cartas[0] < cartas[1]){
-            estado=1;
-        }else{
-            //Player win
-            if(cartas[0] > cartas[1]){
-                estado= 2;
+        //Player win
+        //Si el turno de la maquina es menor que el del jugador
+        if (cartas[2] < cartas[0]) {
+            estado = 2;
+        } else {
+            //Machine win
+            //Si el turno del jugador es menor que el de la maquina
+            if (cartas[0] < cartas[2]) {
+                estado = 1;
                 //Draw
-            }else{
-                definirPorPalo();
+            } else {
+                if (cartas[0] == cartas[2] || cartas[2] == cartas[0]) {
+                    definirPorPalo();
+                }
+
             }
         }
     }
@@ -56,39 +61,52 @@ public class Model {
          * Basto = 4
          */
 
-        if (cartas[1] < cartas[3] ){
+        if (cartas[1] < cartas[3]) {
             estado = 3;
-        }else{
-            estado = 4;
+        } else {
+            if (cartas[1] > cartas[3]) {
+                estado = 4;
+            } else {
+                estado = 5;
+            }
         }
-
     }
 
     public String getEstadoToString() {
-        switch (estado){
+        switch (estado) {
             case 1:
-                estadoToString = "Carta del jugador = "+cartas[0]+"\n" +
-                        "Carta de la máquina = "+cartas[2]+
+                estadoToString = "Carta del jugador = " + cartas[0] + "\n" +
+                        "Carta de la máquina = " + cartas[2] +
                         "\nLa máquina saco una carta de mayor valor, has perdido.";
                 break;
             case 2:
-                estadoToString = "Carta del jugador = "+cartas[0]+"\n" +
-                        "Carta de la máquina = "+cartas[2]+
+                estadoToString = "Carta del jugador = " + cartas[0] + "\n" +
+                        "Carta de la máquina = " + cartas[2] +
                         "\n¡¡Sacaste una carta de mayor valor, has ganado!!";
                 break;
             case 3:
-                estadoToString = "Carta del jugador = "+cartas[0]+"\n" +
-                        "Carta de la máquina = "+cartas[2]+
-                        "Por desempate el jugador ha ganado al obtener un palo mayor al de la máquina. \n" +
+                estadoToString = "Carta del jugador = " + cartas[0] + "\n" +
+                        "Carta de la máquina = " + cartas[2] +
+                        "\nPor desempate el jugador ha ganado al obtener un palo mayor al de la máquina. \n" +
                         "¡¡FELICIDADES!!";
                 break;
             case 4:
-                estadoToString = "Carta del jugador = "+cartas[0]+"\n" +
-                        "Carta de la máquina = "+cartas[2]+
-                        "Por desempate la máquina ha ganado al obtener un palo mayor al del jugador. ";
+                estadoToString = "Carta del jugador = " + cartas[0] + "\n" +
+                        "Carta de la máquina = " + cartas[2] +
+                        "\nPor desempate la máquina ha ganado al obtener un palo mayor al del jugador. ";
+                break;
+
+            case 5:
+                estadoToString = "Carta del jugador = " + cartas[0] + "\n" +
+                        "Carta de la máquina = " + cartas[2] +
+                        "\nEl juego ha quedado empate, ambos jugadores sacaron las mismas caras con los mismos palos. ";
                 break;
         }
         return estadoToString;
+    }
+
+    public int[] getCartas() {
+        return cartas;
     }
 }
 
